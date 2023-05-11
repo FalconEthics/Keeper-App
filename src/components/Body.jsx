65682882c596fb6c691b754d766a6e292aa8d1da
Soumik Das <79033574+FalconEthics@users.noreply.github.com>
2023-05-11 21:14:58 +0530

    added animation

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,14 +2,16 @@ import {CreateNotes} from "./subComponents/CreateNotes.jsx";
 import {DisplayNotes} from "./subComponents/DisplayNotes.jsx";
 import {useContext} from "react";
 import {UserContext} from "../store/UserContext.jsx";
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 export function Body() {
     const {notes} = useContext(UserContext)
+    const [parent] = useAutoAnimate(/* optional config */)
 
     return (<>
         <div className={"w-full flex-col justify-center p-6 space-y-8 mb-10"}>
             <CreateNotes/>
-            <div className={"w-full grid sm:grid-cols-2 lg:grid-cols-3 grid-flow-row gap-4"}>
+            <div className={"w-full grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-flow-row gap-4"} ref={parent}>
                 {notes.map((note, index) => {
                     return <DisplayNotes key={index} id={note.id} time={note.time} title={note.title} note={note.note}/>
                 })}
--- src/components/subComponents/CreateNotes.jsx
+++ src/components/subComponents/CreateNotes.jsx
@@ -1,6 +1,7 @@
 import {AiOutlinePlus} from "react-icons/all.js";
-import {useContext, useReducer, useState} from "react";
+import {useContext, useEffect, useReducer, useRef, useState} from "react";
 import {UserContext} from "../../store/UserContext.jsx";
+import autoAnimate from "@formkit/auto-animate";
 
 const reducer = (state, action) => {
     let value = "";
@@ -36,20 +37,25 @@ export function CreateNotes() {
     const {setNotes} = useContext(UserContext);
     const [state, dispatch] = useReducer(reducer, {id: 0, title: "", note: "", time: ""});
     const [expanded, setExpanded] = useState(false);
+    const parent = useRef(null)
 
     function uploadNote() {
         setNotes((prevNotes) => {
             return [...prevNotes, state]
         });
         dispatch({type: "clean"});
-        console.log(state);
     }
+
+    useEffect(() => {
+        parent.current && autoAnimate(parent.current, {duration: 300})
+    }, [parent])
 
     return (<>
         <form className={"relative flex justify-center"} onSubmit={() => {
             event.preventDefault();
         }}>
             <div
+                ref={parent}
                 className={"flex flex-col w-full sm:w-2/3 lg:w-2/5 drop-shadow-2xl bg-white p-4 rounded-2xl"}>
                 <input name={"title"} value={state.title} type="text" placeholder={"Title"} maxLength={"50"}
                        className={"p-1 rounded-lg focus:outline-none outline-amber-200"}
@@ -59,12 +65,13 @@ export function CreateNotes() {
                        onChange={() => {
                            dispatch({type: "saveTitleChanges"});
                        }}/>
-                <textarea name={"note"} value={state.note} placeholder={"Write your Note here!"} spellCheck={"true"}
-                          className={`focus:outline-none outline-amber-200 rounded-lg transition-all duration-300 ${expanded ? "h-24 opacity-100 mt-2 p-1" : "h-0 opacity-0 p-0 m-0"}`}
-                          onChange={() => {
-                              dispatch({type: "saveNoteChanges"});
-                          }}>
-                </textarea>
+                {expanded &&
+                    <textarea name={"note"} value={state.note} placeholder={"Write your Note here!"} spellCheck={"true"}
+                              className={"focus:outline-none outline-amber-200 rounded-lg h-24 opacity-100 mt-2 p-1"}
+                              onChange={() => {
+                                  dispatch({type: "saveNoteChanges"});
+                              }}
+                    ></textarea>}
                 <button
                     className={"rounded-full bg-amber-300 w-fit p-2 hover:bg-amber-200 text-2xl absolute bottom-[-13%] right-8 text-white hover:text-black hover:scale-150"}
                     onClick={() => {
